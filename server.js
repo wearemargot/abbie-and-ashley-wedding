479d2988b72e75513a806203d40f5ea9b46585a4
ashley-wheat <ashley.wheat@digital.justice.gov.uk>
2024-05-14 09:53:49 +0100

    Removing public directory for static site to work

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-app.use(express.static("public"));
+app.use(express.static("./"));
 
 app.get('/', (req, res) => {
   res.sendFile('index.html');
